5d1474b78b4bcdab3b7508f3363afafc14dc411f
ignagim <ignagimenez10@gmail.com>
2022-04-28 17:14:44 -0300

    final commit

--- webpack.config.js
+++ webpack.config.js
@@ -7,15 +7,16 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js',
-		publicPath: './'
+		publicPath: '/'
 	},
 	mode: 'development',
 	resolve: {
 		extensions: ['.js', '.jsx'],
 		alias: {
-			'@components': path.resolve(__dirname, 'src/components'),
-			'@containers': path.resolve(__dirname, 'src/containers'),
+			'@components': path.resolve(__dirname, 'src/components/'),
+			'@containers': path.resolve(__dirname, 'src/containers/'),
 			'@pages': path.resolve(__dirname, 'src/pages/'),
+			'@routes': path.resolve(__dirname, 'src/routes/'),
 			'@styles': path.resolve(__dirname, 'src/styles/'),
 			'@icons': path.resolve(__dirname, 'src/assets/icons/'),
 			'@logos': path.resolve(__dirname, 'src/assets/logos/'),
@@ -61,10 +62,7 @@ module.exports = {
 			filename: '[name].css'
 		}),
 	],
-    devServer: {
+	devServer: {
 		historyApiFallback: true,
-        allowedHosts: path.join(__dirname, 'dist'),
-        port: 3005,
-        compress: true,
-    }
+	}
 }
